3df3be05af9d4286680ccb79635425dc2e5154d9
Melo <lcmelo@koerich.com.br>
2021-11-10 18:51:45 -0300

    Inserido spring-boot no projeto

---
+++ src/main/java/org/thoughts/on/java/coffee/CoffeeMachineApplication.java
@@ -0,0 +1,12 @@
+package org.thoughts.on.java.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeMachineApplication.class, args);
+    }
+}
