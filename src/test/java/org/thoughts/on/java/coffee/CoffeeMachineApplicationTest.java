fb3e53a033f4be39d477326f624a293c2201e415
Melo <lcmelo@koerich.com.br>
2021-11-10 20:00:12 -0300

    Ajustada classe teste e pom

---
+++ src/test/java/org/thoughts/on/java/coffee/CoffeeMachineApplicationTest.java
@@ -0,0 +1,11 @@
+package org.thoughts.on.java.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMachineApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
